d2388ec718257c7ea6b4c15baf427680e1aa965b
alissa <alissanishihara@gmail.com>
2022-07-26 23:54:11 +0200

    create components folder

---
+++ frontend/src/components/example.js
@@ -0,0 +1,5 @@
+/*
+This file is just to explains what we need to put inside components folder. You can delete it.
+
+I this folder we need to put this files: home, login, signup, form page and Find your pet page.
+*/
