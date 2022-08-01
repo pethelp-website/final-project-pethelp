934e5a209102e0ba964a058d2afbce0503ad850f
isha789 <isha.waheed1810@gmail.com>
2022-08-01 17:40:24 +0200

    Login-Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoginForm from "./components/login/LoginForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const adminUser = {
+    email: "",
+    password: ""
+  }
+
+  const [user, setUser] = useState({ email: "", password:""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if(details.email === adminUser.email && details.password === adminUser.password){
+    console.log("Logged in");
+    setUser({
+     
+      email: details.email
+    });
+  } else {
+//     console.log("Details do not match!"); 
+setError("Details do not match!")
+}
+  }
+  const Logout = () => {
+    setUser({ email:""});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !== "") ? (
+        <div className="welcome">
+          <h2>welcome</h2>
+          <button onClick={Logout}>Logout</button>
+          </div>
+      ) : (
+        <LoginForm   Login={Login}  error={error}/>
+      )}
     </div>
   );
 }
 
 export default App;
+//error={error}
---
+++ frontend/src/components/login/LoginForm.js
@@ -0,0 +1,63 @@
+import React, {useState}  from 'react'
+import { getLocalStorageInfo } from '../../utils/getLocalStorageInfo';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+function LoginForm({Login, error}) {
+    const [details, setDetails] = useState({ email:"", password:"" });
+
+    const submitHandler = e => {
+      fetch("http://localhost:3000/user/sign-in/"
+      , {
+        method: "POST",
+        headers: {
+            authorization: getLocalStorageInfo(),
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: details.email,
+            password: details.password,
+        }),
+    })
+        .then(response => response.json())
+        .then((data) => {
+            localStorage.setItem("token", data.jwtToken);
+        console.log(data);
+        })
+        .catch((error) => console.log(error));
+
+        e.preventDefault();
+Login(details);
+}
+
+return (
+<Form  onSubmit={submitHandler}>
+    <h3>Log In your account!</h3>
+    {(error !== "") ? ( <div className="error">{error}</div> ) : ""}
+<Form.Group className="mb-3" controlId="formBasicEmail">
+  <Form.Label>Email address</Form.Label>
+  <Form.Control type="email" placeholder="Enter email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
+  <Form.Text className="text-muted">
+    
+  </Form.Text>
+</Form.Group>
+
+<Form.Group className="mb-3" controlId="formBasicPassword">
+  <Form.Label>Password</Form.Label>
+  <Form.Control type="password" placeholder="Password"  onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+</Form.Group>
+{/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  <Form.Check type="checkbox" label="Check me out" />
+</Form.Group> */}
+
+<Button variant="primary" type="submit" value="Login" >
+  Log In 
+</Button>
+
+</Form>
+  )
+}
+
+export default LoginForm;
+
