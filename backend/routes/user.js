53475002e5ba947054b1db29adfb6f1ac45ac67e
gabiarno <gabiarno@hotmail.com>
2022-08-04 01:42:37 +0200

    signin function

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,112 +1,107 @@
-
 const express = require("express");
-const { Client } = require('pg');
-const bcrypt = require("bcrypt");   // bcrypt is used to hash password before saving it to database
-const fs = require("fs");   // fs is node's inbuilt file system module used to manage files
+const { Client } = require("pg");
+const bcrypt = require("bcrypt"); // bcrypt is used to hash password before saving it to database
+const fs = require("fs"); // fs is node's inbuilt file system module used to manage files
 
 const generateJWT = require("./generateJWT");
-const client = require('../utils/openConnection');
+const client = require("../utils/openConnection");
 
-const router = express.Router();   // we create a new router using express's inbuilt Router method
-/*
+const router = express.Router(); // we create a new router using express's inbuilt Router method
+
+const INSERT_QUERY =
+  "INSERT INTO users (name, email, address, city, postcode, password) VALUES($1, $2, $3, $4, $5, $6)";
+const SELECT_QUERY = "SELECT * from users WHERE (email=$1)";
+
+const clientDB = new Client(client());
+
+require("dotenv").config(); // here we use dotenv module which we installed in the begining to access environment variables from .env file
+
+// Creates a data base connection with info containing in env
 const connectionData = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'dbnodeauthprojectmigracode',
-    password: 'postgres',
-    port: 5432,
-  }
-  const client = new Client(connectionData);
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+const client1 = new Client(connectionData);
 
-  client.connect();*/
+client1.connect();
 
-  /*const insert = user => {
-    const query = "INSERT INTO Users_proj (name, username, password) VALUES($1, $2, $3)";
-    client.query(query, [user.name, user.email, user.password], (error, response)=> {
-      if (error) {
-        console.log("Something went wrong" + error);
-        return "KO";
-      }
-      
-    });
-        
-        return "OK";
-  }
-
-  const select = email => {
-    const query = "SELECT * from Users_proj WHERE (username=$1)";
-    client.query(query, [email], (error, response)=> {
-      if (error) {
-        console.log("Something went wrong" + error);
-        return "KO";
-      }
-      console.log(response.rows);
-      return response.rows;
-      
-    });
-        
-  }
-*/
+const select = (email) => {
+  client1.query(SELECT_QUERY, [email], (error, response) => {
+    if (error) {
+      console.log("Something went wrong. " + error);
+      return "KO";
+    }
+    console.log(response.rows);
+    return response.rows;
+  });
+};
 
 // user registration / sign-up
 router.post("/sign-up", async (req, res) => {
-  const {  name, email, password } = req.body;
+  const { name, email, address, city, postcode, password } = req.body;
   console.log("en signup");
 
   try {
     const user = "";
-    //await usersDb.filter(user => user.email === email);
-
-    if (user.length > 0) {
-      return res.status(400).json({error: "User already exist!"});
-    }
-
+    
     const salt = await bcrypt.genSalt(10);
     const bcryptPassword = await bcrypt.hash(password, salt);
 
-    let newUser = {
-      name: name,
-      email: email,
-      password: bcryptPassword
-    }
+    // Check user don't exists
+    client1.query(SELECT_QUERY, [email], (error, response) => {
+      if (error) {
+        console.log("Something went wrong. " + error);
+        
+      }
+      console.log("length",response.rows.length);
+      if (response.rows.length > 0) {
+        
+        return res.status(400).json({ error: "User already exist!" });
+      } else {
 
-    // we add newUser to usersDb 
-    ////////////////////////////////////////////insert(newUser);
-
-
+       
     
+        // we add newUser to usersDb
+        client1.query(
+          INSERT_QUERY,
+          [name, email, address, city, postcode, password],
+          (error, response) => {
+            if (error) {
+              console.log("Something went wrong" + error);
+              // return "error";
+            }
+            if (response) {
+              console.log(response);
+              /* Once the user registration is done successfully, we will generate a
+                jsonwebtoken and send it back to user. This token will be used for
+                accessing other resources to verify identity of the user.
+                
+                The following generateJWT function does not exist till now but we
+                will create it in the next step. */
     
-    /* Once the user registration is done successfully, we will generate a
-      jsonwebtoken and send it back to user. This token will be used for
-      accessing other resources to verify identity of the user.
+              const jwtToken = generateJWT(email);
+    
+              return res
+                .status(201)
+                .send({ jwtToken: jwtToken, isAuthenticated: true });
+            }
+            //console.log("response: ", response);
+            //return "OK";
+          }
+        );
+      }
       
-      The following generateJWT function does not exist till now but we
-      will create it in the next step. */
+    });
+
     
-    const jwtToken = generateJWT(newUser.id);
-
-    return res.status(201).send({ jwtToken: jwtToken, isAuthenticated: true});
-
   } catch (error) {
     console.error(error.message);
-    res.status(500).send({error: error.message});
+    res.status(500).send({ error: error.message });
   }
 });
-
-/*router.get("/sign", async (req, res) => {
-    /*client.query("select * from public.users_proj")
-        .then(response => {
-            console.log("response: ",response.rows);
-            //client.end()
-        })
-        .catch(err => {
-            console.log("Error BD  get", err);
-           // client.end()get
-        });
-    res.status(200).json("OK");
-});
-*/
-
 
 // user sign-in / login
 router.post("/sign-in", async (req, res) => {
@@ -144,17 +139,15 @@ router.post("/sign-in", async (req, res) => {
           
         }
       });*/
-          
-      const jwtToken = generateJWT(email);
-      
-      res.status(200).send({ jwtToken, isAuthenticated: true });
-    // if the user exist then we will compare the password provided by user with the hashed password we stored during user registration
 
+    const jwtToken = generateJWT(email);
+
+    res.status(200).send({ jwtToken, isAuthenticated: true });
+    // if the user exist then we will compare the password provided by user with the hashed password we stored during user registration
   } catch (error) {
     console.error(error.message);
-    res.status(500).send({error: error.message});
+    res.status(500).send({ error: error.message });
   }
 });
 
-
-module.exports = router;   // we need to export this router to implement it inside our server.js file
+module.exports = router; // we need to export this router to implement it inside our server.js file
