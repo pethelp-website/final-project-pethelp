94084772983b930b4702d45f17749094128f3398
alissa <alissanishihara@gmail.com>
2022-08-04 13:53:45 +0200

    improve navbar

--- frontend/src/components/home/homeheader/HomeHeader.jsx
+++ frontend/src/components/home/homeheader/HomeHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav} from 'react-bootstrap';
 import "./HomeHeader.scss";
 import logo from "../../../images/logo.png";
 import { Link } from "react-router-dom";
@@ -8,15 +8,24 @@ import { Link } from "react-router-dom";
 function HomeHeader() {
   return (
     <div>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand as={Link} to={"/"} > <img src={logo} alt="logo"/> PetHelp</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to={"/login"} >Login</Nav.Link>
-            <Nav.Link as={Link} to={"/sign-up"}>Signup</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand as={Link} to={"/"}>
+              <img
+                alt=""
+                src={logo}
+                className="d-inline-block align-top"
+              />{' '}
+              PetHelp
+            </Navbar.Brand>
+            <Nav id="nav-links" className="ml-auto">
+              <Nav.Link as={Link} to={"/login"} >Login</Nav.Link>
+              <Nav.Link as={Link} to={"/sign-up"}>Signup</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
     </div >
   );
 }
