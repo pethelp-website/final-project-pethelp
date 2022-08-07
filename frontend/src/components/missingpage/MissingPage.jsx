7a1747f5262645830a02066d5649de05b0ae2d22
alissa <alissanishihara@gmail.com>
2022-08-07 18:39:49 +0200

    implement login validation if password or login does not match

--- frontend/src/components/missingpage/MissingPage.jsx
+++ frontend/src/components/missingpage/MissingPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MissingPage = () => {
   return (
     <div>
-      <h1>This is the missing page!</h1>
+      <h1>This is the missing page...</h1>
     </div>
   )
 }
