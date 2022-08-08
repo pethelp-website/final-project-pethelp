1d804aa3de2b55cda1d22d19fe659036f8db7ecb
alissa <alissanishihara@gmail.com>
2022-08-08 12:49:23 +0200

    change main part text

--- frontend/src/components/home/homeheader/HomeHeader.jsx
+++ frontend/src/components/home/homeheader/HomeHeader.jsx
@@ -8,30 +8,31 @@ import { Link } from "react-router-dom";
 function HomeHeader() {
   return (
     <div>
-      <Navbar  variant="dark" className="navbar sticky-top" expand="lg">
-          <Container>
-            <Navbar.Brand as={Link} to={"/"}>
-              <img
-                alt=""
-                src={logo}
-                className="d-inline-block align-top"
-              />{' '}
-              PetHelp
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-              <Nav id="nav-links">
-                <Nav.Link as={Link} to={"/login"} >Login</Nav.Link>
-                <Nav.Link as={Link} to={"/sign-up"}>Signup</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+      <Navbar variant="dark" className="navbar sticky-top" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to={"/"}>
+            <img
+              id="logo"
+              alt=""
+              src={logo}
+              className="d-inline-block align-top"
+            />{' '}
+            PetHelp
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav id="nav-links">
+              <Nav.Link as={Link} to={"/login"} >Login</Nav.Link>
+              <Nav.Link as={Link} to={"/sign-up"}>Signup</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
 
 
-      </div >
+    </div >
   );
 }
 
-      export default HomeHeader;
+export default HomeHeader;
