b1cdc1bd9512ecf5f66e5770c8e1cbec02a3d0ec
Yuxuan <nine.slide@gmail.com>
2022-08-14 21:20:06 +0200

    footer-header

--- frontend/src/components/home/homeheader/HomeHeader.jsx
+++ frontend/src/components/home/homeheader/HomeHeader.jsx
@@ -3,27 +3,28 @@ import { Container, Navbar, Nav } from 'react-bootstrap';
 import "./HomeHeader.scss";
 import logo from "../../../images/logo.png";
 import { Link } from "react-router-dom";
-
+import './HomeHeader.scss';
+import '../../../style/style.scss';
 
 function HomeHeader() {
   return (
     <div>
       <Navbar variant="dark" className="navbar sticky-top" expand="lg">
         <Container>
-          <Navbar.Brand as={Link} to={"/"}>
+          <Navbar.Brand as={Link} to={"/"} className='navbar__title'>
             <img
               id="logo"
               alt=""
               src={logo}
-              className="d-inline-block align-top"
+              className="d-inline-block align-top navbar__logo"
             />{' '}
-            PetHelp
+            Pet Help
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav id="nav-links">
-              <Nav.Link as={Link} to={"/login"} >Login</Nav.Link>
-              <Nav.Link as={Link} to={"/sign-up"}>Signup</Nav.Link>
+              <Nav.Link as={Link} to={"/login"} classname='navbar__logIn' >Login</Nav.Link>
+              <Nav.Link as={Link} to={"/sign-up"} className='navbar__signUp'>Signup</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
