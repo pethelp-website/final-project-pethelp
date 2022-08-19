1355c0bc93f7a081840ef530e50773a81758cb52
ppalladio <nine.slide@gmail.com>
2022-08-19 20:58:27 +0200

    footer bgcolor and font color

--- frontend/src/components/home/homefooter/HomeFooter.jsx
+++ frontend/src/components/home/homefooter/HomeFooter.jsx
@@ -5,14 +5,14 @@ import "./HomeFooter.scss";
 import faceLogo from "../../../images/facebook (3).png";
 import instaLogo from "../../../images/instagram (1).png";
 import twitterLogo from "../../../images/twitter.png";
-
+import '../../../style/style.scss';
 
 
 const HomeFooter = () => {
   return (
     <div id="container">
 
-      <CDBFooter className="shadow fixed-bottom" id="footer">
+      <CDBFooter className="shadow fixed-bottom " id="footer">
         <CDBBox
           display="flex"
           justifyContent="between"
@@ -27,10 +27,11 @@ const HomeFooter = () => {
           </CDBBox>
           <CDBBox display="flex">
 
-
-          <CDBBox display="flex"  className="p-2">
+          <div className="logo-box">
+          <CDBBox display="flex"  className="p-2  ">
           <a href="https://www.facebook.com/" target="blank" className="d-flex align-items-end p-0">
             <img
+            className='logo-box__facebook'
               alt="face-logo"
               src={faceLogo}
               width="24px"
@@ -39,7 +40,7 @@ const HomeFooter = () => {
         </CDBBox>
         <CDBBox display="flex"  className="p-2">
           <a href= "https://www.instagram.com/" target="blank" className="d-flex align-items-end p-0">
-            <img
+            <img className='logo-box__ins'
               alt="insta-logo"
               src={instaLogo}
               width="24px"
@@ -49,7 +50,7 @@ const HomeFooter = () => {
 
         <CDBBox display="flex"  className="p-2">
           <a href="https://www.twitter.com/" target="blank" className="d-flex align-items-end p-0">
-            <img
+            <img className='logo-box__twitter'
               alt="twitter-logo"
               src={twitterLogo}
               width="24px"
@@ -57,7 +58,7 @@ const HomeFooter = () => {
           </a>
         </CDBBox>
 
-           
+        </div>
 
 
 
