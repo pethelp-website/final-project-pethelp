a7a124c9165d694be87f1ba660fdc04d25dd48ca
alissa <alissanishihara@gmail.com>
2022-09-05 18:53:03 +0200

    implement logout endpoint

--- frontend/src/components/userspage/adm/AdmPage.jsx
+++ frontend/src/components/userspage/adm/AdmPage.jsx
@@ -8,7 +8,7 @@ import GetReports from "./reportdata/GetReports";
 const AdmPage = () => {
   return (
     <div>
-      <h1 className="mt-5 heading-secondary">Admin Dashboard</h1>
+      <h1 className="heading-secondary">Admin Dashboard</h1>
       <GetReports />
     </div>
   )
