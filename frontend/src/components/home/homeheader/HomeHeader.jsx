251c63f0d66974fa1ae108e543ac420e4fa81ca8
GloriaKwofie <91749390+GloriaKwofie@users.noreply.github.com>
2022-09-06 12:37:44 +0200

    the working homeheader

--- frontend/src/components/home/homeheader/HomeHeader.jsx
+++ frontend/src/components/home/homeheader/HomeHeader.jsx
@@ -1,22 +1,19 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import loginService from "../../../services/loginService";
 import "./HomeHeader.scss";
 import logo from "../../../images/logo.png";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
+
+
 function HomeHeader() {
   let navigate = useNavigate();
 
+
   //logout function
   const endLoginSession = () => {
-    loginService
-      .logout({
-        password: user.password,
-        email: user.email,
-      })
-      .then((data) => {
     loginService.logout({
       password: user.password,
       email: user.email
@@ -25,16 +22,16 @@ function HomeHeader() {
       token: loginService.getToken().jwtToken
     }).then(data => {
 
-
         navigate("/login", { replace: true });
         console.log(data);
-      });
-  }
+      })
+  };
+
 
   //function from services checks if user is logged in
-  
   const isLoggedIn = loginService.isLoggedIn();
 
+  
   //import function from services that checks if the user is admin
   const user = loginService.getUser();
 
@@ -47,48 +44,23 @@ function HomeHeader() {
             alt=""
             src={logo}
             className="d-inline-block align-top navbar__logo"
-          />{" "}
+          />{' '}
           pet help
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav id="nav-links">
-            {isLoggedIn && (
-              <Nav.Link as={Link} to={"/report-page"}>
-                Found a pet
-              </Nav.Link>
-            )}
-            <Nav.Link as={Link} to={"/missing-page"}>
-              Lost a pet
-            </Nav.Link>
-            {!isLoggedIn && (
-              <Nav.Link as={Link} to={"/login"}>
-                Login
-              </Nav.Link>
-            )}
-            {isLoggedIn && !user.isAdmin && (
-              <Nav.Link as={Link} to={"/user-page"}>
-                My account
-              </Nav.Link>
-            )}{" "}
-            {/*user.IsAdmin access this data in user object*/}
-            {isLoggedIn && user.isAdmin && (
-              <Nav.Link as={Link} to={"/adm-page"}>
-                Admin
-              </Nav.Link>
-            )}
-            {!isLoggedIn && (
-              <Nav.Link className="link" as={Link} to={"/sign-up"}>
-                Signup
-              </Nav.Link>
-            )}
-            {isLoggedIn && (
-              <Nav.Link onClick={() => endLoginSession()}>Logout</Nav.Link>
-            )}
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
+          <Nav id="nav-links" >
+            {isLoggedIn && <Nav.Link as={Link} to={"/report-page"}>Found a pet</Nav.Link>}
+            <Nav.Link as={Link} to={"/missing-page"}>Lost a pet</Nav.Link>
+            {!isLoggedIn && <Nav.Link as={Link} to={"/login"} >Login</Nav.Link>}
+            {isLoggedIn && !user.isAdmin &&<Nav.Link as={Link} to={"/user-page"}>My account</Nav.Link>} {/*user.IsAdmin access this data in user object*/}
+            {isLoggedIn && user.isAdmin &&<Nav.Link as={Link} to={"/adm-page"}>Admin</Nav.Link>}
+            {!isLoggedIn && <Nav.Link className='link' as={Link} to={"/sign-up"}>Signup</Nav.Link>}
+            {isLoggedIn && <Nav.Link onClick={() => endLoginSession()}>Logout</Nav.Link>}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-    </div>
+    </div >
   );
 }
 
